cbe860042599b64624dc3226b7f51d29b1d556d4
Malak Tayeh <malaktayeh@outlook.com>
2019-04-27 18:40:50 -0400

    added react-navigation and app pages

--- CookPro/App.js
+++ CookPro/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './pages/HomeScreen';
+import Recipes from './pages/Recipes';
+import Favorites from './pages/Favorites';
+import LoginPage from './pages/LoginPage';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Recipes: {screen: Recipes},
+  Favorites: {screen: Favorites},
+  LoginPage: {screen: LoginPage}
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
